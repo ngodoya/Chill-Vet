695e3fac34c0f5ce1fe3385e1dbe7b7aa18f1b6f
Juan David Salgado Prieto <jsalgadop@unal.edu.co>
2026-06-17 18:41:19 -0500

    Update clsGestor_citas.py

--- services/clsGestor_citas.py
+++ services/clsGestor_citas.py
@@ -1,12 +1,23 @@
-class GestorCitas: 
-    def __init__(self, citas: dict): 
-        self.__citas = citas
-
-    def agendar_cita(self, cita): 
-        print("Agendar cita")
-    def cancelar_cita(self, cita):
-        print("Cancelar cita")
-    def obtener_citas(self, fecha):
-        print("Obtener citas")
-    def obtener_todas(self): 
-        print("Obtener todas las citas") 
+class GestorCitas():
+    def __init__(self):
+        self.citas = {}
+    def agendar_cita(self, cita: dict):
+        cita_id = cita.get("id")
+        if cita_id is None:
+            raise ValueError("La cita debe tener un campo 'id'")
+        if cita_id in self.citas:
+            raise ValueError("Ya existe una cita con id ", cita_id)
+        self.citas[cita_id] = cita
+    def cancelar_cita(self, id: str):
+        if id not in self.citas:
+            raise ValueError("Intente denuevo, no existe una cita con id", id)
+        else: 
+            del self.citas[id]
+    def obtener_cita(self, fecha: str):
+        fecha_cita = []
+        for i in self.citas.values():
+            if i.get("fecha") == fecha:
+                fecha_cita.append(i)
+        return fecha_cita
+    def obtener_todas(self):
+        return list(self.citas.values())
