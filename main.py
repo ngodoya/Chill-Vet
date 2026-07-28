89250a181d2bcc55c4698b6ec255adbe636342de
Nicolás Godoy <ngodoya@unal.edu.co>
2026-07-28 18:37:19 -0500

    ruff format

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import tkinter as tk # type: ignore
+import tkinter as tk  # type: ignore
 from gui.ventana_principal import AppVeterinaria
+
 
 def main():
     app = AppVeterinaria()
     app.mainloop()
+
 
 if __name__ == "_main_":
     main()
