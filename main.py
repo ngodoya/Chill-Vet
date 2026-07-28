6d5a3d163876b1c422d25769d9248d21cf270a91
Nicolás Godoy <ngodoya@unal.edu.co>
2026-07-28 18:35:43 -0500

    Tkinder: PR #26

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import tkinter as tk
-from core.sistema import SistemaVeterinaria
-from gui.ventana_mascotas_duenos import VentanaMascotasDuenos
-
+import tkinter as tk # type: ignore
+from gui.ventana_principal import AppVeterinaria
 
 def main():
-    root = tk.Tk()
-    root.withdraw()  # ocultar ventana raíz
-    sistema = SistemaVeterinaria()
-    ventana = VentanaMascotasDuenos(root, sistema)
-    ventana.protocol("WM_DELETE_WINDOW", root.destroy)
-    root.mainloop()
+    app = AppVeterinaria()
+    app.mainloop()
 
-
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
